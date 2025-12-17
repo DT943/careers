d15bc34de74483dedbdea1142c9e4cd3a44f2860
Ammar-Abdulrahman <Ammarabdulrahmman@gmail.com>
2025-12-17 15:16:10 +0300

    feat: handle responsive modal card with all pages and all content

--- components/ProfileModalShell.tsx
+++ components/ProfileModalShell.tsx
@@ -15,9 +15,13 @@ const ProfileModalShell = ({
   maxWidthClass = "max-w-4xl",
 }: ProfileModalShellProps) => {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 px-4">
-      <div className={`w-full ${maxWidthClass} rounded-lg bg-white shadow-xl`}>
-        <div className="px-6 py-6">{children}</div>
+    <div className="fixed inset-0 z-999 flex items-center justify-center bg-black/50 px-4">
+      <div
+        className={`w-full ${maxWidthClass} max-h-[80vh] rounded-lg bg-white shadow-xl flex flex-col`}
+      >
+        <div className="px-6 py-6 overflow-y-auto flex-1 min-h-0">
+          {children}
+        </div>
 
         {footer && (
           <div className="flex justify-end gap-3 px-6 py-4">{footer}</div>
